2e8b72a5a251269c19082ecc4f7a45c46b9e80f4
RimelBM <66739923+RimelBM@users.noreply.github.com>
2020-07-16 20:36:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from 'react-redux';
-import {createStore} from 'redux';
-import reducer from './reducers/CounterReducer'
-import App from './App'
-import store from './store'
+import App from './App';
+import store from './store';
 
 
 render(
